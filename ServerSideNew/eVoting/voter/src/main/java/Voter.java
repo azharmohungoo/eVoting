71c3de0c500a9d88c68a29904bc552107d8a66ef
gift-sefako <giftsefako@gmail.com>
2016-08-21 17:21:26 +0200

    added required modules

---
+++ ServerSideNew/eVoting/voter/src/main/java/Voter.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Gift on 21/08/16.
+ */
+public class Voter {
+
+    private String userType = "Voter";
+    private String idNum;
+    private String password;
+    private String name;
+    private String surname;
+    private String locationRegistered;
+    private String cellphone;
+    private String email;
+
+    
+}
