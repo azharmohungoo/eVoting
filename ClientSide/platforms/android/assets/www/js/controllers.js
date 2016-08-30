340b59a9d81f76d09d7cd0dbfcf16b2d0a6c3997
Azhar Mohungoo <azhar.m.ish@gmail.com>
2016-08-30 11:33:25 +0200

    No specific changes

---
+++ ClientSide/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,68 @@
+angular.module('app.controllers', [])
+
+.controller('eVotingCtrl', function($scope) {
+
+})
+
+.controller('electionInformationCtrl', function($scope) {
+
+})
+
+.controller('voteNationalCtrl', function($scope) {
+
+  $scope.castVote = function() {
+
+    alert("Congratulations. Your vote has been cast. ")
+  }
+
+})
+
+
+.controller('eVotingLoginCtrl', function($scope, LoginService, $ionicPopup, $state, userService) {
+  $scope.data = {};
+
+  $scope.login = function() {
+    LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+      $state.go('tabsController.electionInformation');
+    }).error(function(data) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials!'
+      });
+    });
+
+
+    }
+
+  $scope.GetUsers = function(){
+    alert('something');
+    userService.GetUsers().then(function(result){
+      //$scope.users = users;
+      console.log(result);
+
+    });
+  }
+
+})
+
+
+
+
+.controller('registerCtrl', function($scope) {
+
+})
+
+.controller('voteProvincialCtrl', function($scope) {
+
+})
+
+.controller('accountInformationCtrl', function($scope) {
+
+})
+
+  .controller('MainCtrl', function($scope, userService) {
+
+
+
+
+  })
---
+++ ClientSide/platforms/android/assets/www/js/routes.js
@@ -0,0 +1,87 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+      .state('eVoting', {
+    url: '/page2',
+    templateUrl: 'templates/eVoting.html',
+    controller: 'eVotingCtrl'
+  })
+
+  .state('tabsController.electionInformation', {
+    url: '/page3',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/electionInformation.html',
+        controller: 'electionInformationCtrl'
+      }
+    }
+  })
+
+    .state('tabsController.viewParty', {
+      url: '/viewParty',
+      views: {
+        'tab1': {
+          templateUrl: 'templates/viewParty.html',
+          //controller: 'electionInformationCtrl'
+        }
+      }
+    })
+
+  .state('tabsController.voteNational', {
+    url: '/page4',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/voteNational.html',
+        controller: 'voteNationalCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('eVotingLogin', {
+    url: '/page5',
+    templateUrl: 'templates/eVotingLogin.html',
+    controller: 'eVotingLoginCtrl'
+  })
+
+  .state('register', {
+    url: '/page6',
+    templateUrl: 'templates/register.html',
+    controller: 'registerCtrl'
+  })
+
+  .state('tabsController.voteProvincial', {
+    url: '/page7',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/voteProvincial.html',
+        controller: 'voteProvincialCtrl'
+      }
+    }
+  })
+
+  .state('accountInformation', {
+    url: '/page8',
+    templateUrl: 'templates/accountInformation.html',
+    controller: 'accountInformationCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+
+
+});
