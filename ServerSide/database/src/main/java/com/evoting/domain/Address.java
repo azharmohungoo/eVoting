c3b049ef2a80d1c1e2f7ef04ea5214ed3ae05aac
Azhar Mohungoo <azhar.m.ish@gmail.com>
2016-09-04 18:53:42 +0200

    Changes to Address table

--- ServerSide/database/src/main/java/com/evoting/domain/Address.java
+++ ServerSide/database/src/main/java/com/evoting/domain/Address.java
@@ -14,12 +14,15 @@ import javax.persistence.*;
 public class Address {
     private int id;
     private String nodeName;
-    private String blockchainAddress;
     private String ipAddress;
+    private String port;
+    private String rpcUsername;
+    private String rpcPassword;
+    private String blockchainAddress;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", unique = true, nullable = false)
+    @Column(name = "id", nullable = false)
     public int getId() {
         return id;
     }
@@ -27,7 +30,8 @@ public class Address {
     public void setId(int id) {
         this.id = id;
     }
-    @Column(name = "node_name", unique = true, nullable = false)
+
+    @Column(name = "node_name", nullable = false)
     public String getNodeName() {
         return nodeName;
     }
@@ -35,20 +39,49 @@ public class Address {
     public void setNodeName(String nodeName) {
         this.nodeName = nodeName;
     }
-    @Column(name = "blockchain_address", unique = true, nullable = false)
-    public String getBlockchainAddress() {
-        return blockchainAddress;
-    }
 
-    public void setBlockchainAddress(String blockchainAddress) {
-        this.blockchainAddress = blockchainAddress;
-    }
-    @Column(name = "ip_address", unique = true, nullable = false)
+    @Column(name = "ip_address", nullable = false)
     public String getIpAddress() {
         return ipAddress;
     }
 
     public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
+    }
+
+    @Column(name = "port", nullable = false)
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    @Column(name = "rpc_username", nullable = false)
+    public String getRpcUsername() {
+        return rpcUsername;
+    }
+
+    public void setRpcUsername(String rpcUsername) {
+        this.rpcUsername = rpcUsername;
+    }
+
+    @Column(name = "rpc_password", nullable = false)
+    public String getRpcPassword() {
+        return rpcPassword;
+    }
+
+    public void setRpcPassword(String rpcPassword) {
+        this.rpcPassword = rpcPassword;
+    }
+
+    @Column(name = "blockchain_address", nullable = false)
+    public String getBlockchainAddress() {
+        return blockchainAddress;
+    }
+
+    public void setBlockchainAddress(String blockchainAddress) {
+        this.blockchainAddress = blockchainAddress;
     }
 }
