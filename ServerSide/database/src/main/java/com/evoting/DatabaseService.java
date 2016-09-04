4bbc1442018d86d78c1c663fe651f175f68c9102
Azhar Mohungoo <azhar.m.ish@gmail.com>
2016-09-04 10:05:49 +0200

    No specific changes.. again

--- ServerSide/database/src/main/java/com/evoting/DatabaseService.java
+++ ServerSide/database/src/main/java/com/evoting/DatabaseService.java
@@ -24,13 +24,10 @@ public class DatabaseService
             return false;
         }
 
-<<<<<<< HEAD
-=======
         if (p.getIdNum().equals(_p.getIdNum()) && p.getPassword().equals(_p.getPassword()))
         {
             return true;
         }
->>>>>>> e5d79075944d184b4ca2a67842142c0cfa2d0fe3
 
         return false;
     }
